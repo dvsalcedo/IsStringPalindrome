3e19655b93087cdab97af064623fd1bb8bc8a4ca
David S <dvsalcedov@gmail.com>
2020-11-22 17:41:38 -0500

    New module to check the process

--- IsStringPalindrome/Program.cs
+++ IsStringPalindrome/Program.cs
@@ -6,31 +6,34 @@ namespace IsStringPalindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Write a sentence to check wheter palindrome or not");
+            string textToEvaluate = Console.ReadLine();
+            Console.WriteLine(IsPalindrome(textToEvaluate) ? "Is palindrome" : "Is not Palindrome");
+            Console.ReadLine();
+        }
+
+        public static bool IsPalindrome(string word)
+        {
             bool isPalindrome = true;
-            string textToEvaluate = "tet";
-            char[] chars = textToEvaluate.ToCharArray();
+            char[] chars = word.Replace(" ", "").ToCharArray();
             int[] NumberOfAppears = new int[char.ToUpper('z') - char.ToUpper('a') + 1];
 
-            for (int i=0; i<chars.Length; i++)
+            for (int i = 0; i < chars.Length; i++)
             {
                 int numberOfChar = char.ToUpper(chars[i]) - 64;
-                if((char.ToUpper('a') - 64) <= numberOfChar && numberOfChar <= (char.ToUpper('z')-64))
+                if ((char.ToUpper('a') - 64) <= numberOfChar && numberOfChar <= (char.ToUpper('z') - 64))
                 {
                     NumberOfAppears[numberOfChar]++;
                 }
-                else
-                {
-                    NumberOfAppears[numberOfChar] = -1;
-                }
+
             }
 
             bool odd = false;
             foreach (var number in NumberOfAppears)
             {
-                if(number % 2 == 1)
+                if (number % 2 == 1)
                 {
-                    if (odd)
+                    if (odd) //Two or more odds
                     {
                         isPalindrome = false;
                         break;
@@ -39,15 +42,7 @@ namespace IsStringPalindrome
                 }
             }
 
-            if (isPalindrome)
-            {
-                Console.WriteLine("Is Palindrome");
-            }
-            else
-            {
-                Console.WriteLine("Is NOT Palindrome");
-            }
-
+            return isPalindrome;
         }
     }
 }
