0cae11b4660defab2bec3b104f99047cba6dda29
David Salcedo <dvsalcedov@gmail.com>
2021-01-11 14:02:56 -0500

    Fixed code with stack

--- IsStringPalindrome/Program.cs
+++ IsStringPalindrome/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IsStringPalindrome
 {
     class Program
     {
-
-        const int LIMITCHARS = 128; //128 o 256 ASCCI
-
         static void Main(string[] args)
         {
             Console.WriteLine("Write a sentence to check wheter palindrome or not");
@@ -17,28 +15,34 @@ namespace IsStringPalindrome
 
         public static bool IsPalindrome(string word)
         {
-            char[] chars = word.ToLower().Replace(" ", "").ToCharArray();
-            int[] NumberOfAppears = new int[LIMITCHARS];
+            char[] chars = word.ToLower().ToCharArray();
+            Stack<int> pile = new Stack<int>();
+            int isMiddle = (chars.Length % 2 == 1) ? (chars.Length / 2) : (chars.Length / 2) - 1;
+            bool odd = chars.Length % 2 == 1;
 
             for (int i = 0; i < chars.Length; i++)
             {
-                NumberOfAppears[(int)chars[i]]++;
-            }
-
-            bool odd = false;
-            foreach (var number in NumberOfAppears)
-            {
-                if (number % 2 == 1)
+                if (i <= isMiddle)
                 {
-                    if (odd) //Two or more odds
+                    pile.Push(chars[i]);
+                }
+
+                if (i > isMiddle)
+                {
+                    if (odd)
+                    {
+                        pile.Pop();
+                        odd = false;
+                    }
+                    if (chars[i] != pile.Pop())
                     {
                         return false;
                     }
-                    odd = true;
                 }
+
             }
 
             return true;
-        }
+        }  
     }
 }
